23588d1c08e15ebe9a1a0a3b54fa131d0bdc735f
KerkkaiwanMam <kerkkaiwan.ks@gmail.com>
2023-08-20 22:54:50 +0700

    exception
    -  ตรวจสอบว่า role
    - findbyemail

--- src/utils/role.enum.ts
+++ src/utils/role.enum.ts
@@ -1,4 +1,18 @@
 export enum RoleName {
 	Admin = 'admin',
 	Member = 'member',
+	 
+	HeadOfYear = 'headOfYear',
+	Secretary = 'secretary',
+	HeadOfAcademic = 'headofacademic',
+	Academic = 'academic',
+	HeadOfAdministration  = 'headofadministration',
+	Administration= 'administration',
+	HeadOfStation  = 'headofstation',
+	Station= 'station',
+	HeadOfAccountant= 'headofaccountant',
+	Accountant='accountant',
+	Alumni= 'alumni',
+	Advisor= 'advisor'
+	
 }
